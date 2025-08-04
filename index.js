f31a71c3dfb1b390a5245d12d811ba12071cdc4a
Sneha-datla <datla.sneha1@gmail.com>
2025-08-04 18:06:06 +0530

    changes made

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use('employee', employeeRoutes);
+app.use('/employee', employeeRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
