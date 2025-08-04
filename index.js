19ad1fa00a7218c5ec61d92beeaec3b003e154a4
Sneha-datla <datla.sneha1@gmail.com>
2025-08-04 17:55:58 +0530

    changes made

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 // index.js
+require('dotenv').config(); // Load env vars
+
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser');
 const employeeRoutes = require('./routes/employee');
 
 const app = express();
 const PORT = 5000;
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 app.use('employee', employeeRoutes);
 
 app.listen(PORT, () => {
