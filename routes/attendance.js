5d9b62c733d11e356bcd7df4e5a8fbfdc5194982
Sneha-datla <datla.sneha1@gmail.com>
2025-08-16 11:15:27 +0530

    changes made

--- routes/attendance.js
+++ routes/attendance.js
@@ -78,8 +78,8 @@ router.post('/verify-face', upload.single('image'), async (req, res) => {
 });
 
 // Office coordinates (example)
-const OFFICE_LAT =  14.6832402;
-const OFFICE_LNG =  77.5638584;
+const OFFICE_LAT =  17.6672;
+const OFFICE_LNG =  83.2051;
 const RADIUS_IN_METERS = 1000; // Acceptable distance
 
 function getDistanceFromLatLonInMeters(lat1, lon1, lat2, lon2) {
