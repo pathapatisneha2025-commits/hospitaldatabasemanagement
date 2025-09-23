1ba963eb7f6b531b8ca5fdba3824bdc4a7fa3f9e
Sneha-datla <datla.sneha1@gmail.com>
2025-09-23 18:00:09 +0530

    changes made initial commit

--- routes/AdminApis/attendancebreak.js
+++ routes/AdminApis/attendancebreak.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const pool = require("../db"); // PostgreSQL connection
+const pool = require("./db"); // PostgreSQL connection
 const router = express.Router();
 
 // =========================
