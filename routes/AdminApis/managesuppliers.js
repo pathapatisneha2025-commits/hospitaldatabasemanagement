606ee954e5764b9e421a2731a26c56d8cd279dea
Sneha-datla <datla.sneha1@gmail.com>
2025-09-26 21:17:47 +0530

    changes made initial commit

--- routes/AdminApis/managesuppliers.js
+++ routes/AdminApis/managesuppliers.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const pool = require("../db../db");
+const pool = require("../../db");
 
 // -------------------- CREATE Supplier --------------------
 router.post("/add", async (req, res) => {
