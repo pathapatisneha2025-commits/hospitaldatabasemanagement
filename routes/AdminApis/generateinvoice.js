a88a0270ebd822037f47cbb71e83ee0790a881aa
Sneha-datla <datla.sneha1@gmail.com>
2025-09-30 13:14:58 +0530

    changes made initial commit

--- routes/AdminApis/generateinvoice.js
+++ routes/AdminApis/generateinvoice.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const PDFDocument = require('pdfkit');
 const pool = require("../../db"); 
-const authenticateJWT = require('../../middleware/authenticateJWT'); // adjust path
+const authenticateJWT = require('../middleware/auth');
 
 const router = express.Router();
 
