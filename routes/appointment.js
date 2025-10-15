57b00a3048f2a69418c7a49cf9f6e39a29aaa76c
Sneha-datla <datla.sneha1@gmail.com>
2025-10-15 10:53:59 +0530

    changes made initial commit

--- routes/appointment.js
+++ routes/appointment.js
@@ -174,7 +174,7 @@ router.get('/patient/:patientId', async (req, res) => {
 router.get('/doctor/:doctorId', async (req, res) => {
   try {
     const result = await db.query(
-      `SELECT id, doctorId, doctorName, yearsOfExperience, department, date, timeSlot, consultantFees, paymentStatus,status, patientId, name, age, gender, bloodGroup, reason 
+      `SELECT id, doctorId, doctorName, yearsOfExperience, department, date, timeSlot, consultantFees, paymentStatus,status, patientId, name, age, gender, bloodGroup, reason ,tokenid
        FROM appointments 
        WHERE doctorId = $1 
        ORDER BY createdAt DESC`,
