1a07a5542505a98b5162fd078576661738bcf34b
Sneha-datla <datla.sneha1@gmail.com>
2025-11-24 17:51:06 +0530

    changes made

--- routes/AdminApis/pickerapis.js
+++ routes/AdminApis/pickerapis.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const pool = require("../db../db"); // make sure this is your PostgreSQL pool
+const pool = require("../../db"); // make sure this is your PostgreSQL pool
 
 // POST /assign-picker
 router.post("/assign-picker", async (req, res) => {
