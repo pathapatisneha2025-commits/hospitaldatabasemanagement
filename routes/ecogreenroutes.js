b60ceb605632279674faa9243f2b483b1ff838f7
Sneha-datla <datla.sneha1@gmail.com>
2026-04-10 17:26:14 +0530

    changes made

--- routes/ecogreenroutes.js
+++ routes/ecogreenroutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 const multer = require("multer");
 const path = require("path");
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
-const cloudinary = require("../config/cloudinary");
+const cloudinary = require("../cloudinary");
 
 /* ---------------- CLOUDINARY STORAGE ---------------- */
 const storage = new CloudinaryStorage({
