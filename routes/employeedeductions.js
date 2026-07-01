0506cbebd10e43994b329adf695297939b5cfcd3
Sneha-datla <datla.sneha1@gmail.com>
2026-07-01 15:48:22 +0530

    changes made

--- routes/employeedeductions.js
+++ routes/employeedeductions.js
@@ -6,10 +6,7 @@ const pool = require("../db");
 
 // Add new employee deduction
 router.post("/add", async (req, res) => {
-    console.log("REQ BODY:", req.body);
-
   const {
-       //  NEW
     employee_name,
     email,
     salary,
@@ -18,18 +15,16 @@ router.post("/add", async (req, res) => {
     working_days,
     working_hours,
     employee_type,
-    employee_id,   
+    employee_id   // added new field
   } = req.body;
-  console.log("Employee ID:", employee_id);
 
   try {
     const result = await pool.query(
       `INSERT INTO employee_deductions 
-      ( employee_name, email, salary, late_penalty, break_penalty, working_days, working_hours, employee_type,employee_id)
+      (employee_name, email, salary, late_penalty, break_penalty, working_days, working_hours, employee_type,employee_id)
       VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9)
       RETURNING *`,
       [
-        employee_id,     // added first
         employee_name,
         email,
         salary,
